48ffb2efecf12d2283ad74fa4835e88541a54f96
nitao4648 <68411955+nitao4648@users.noreply.github.com>
2025-09-23 18:32:57 +0000

    config auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';  // ensure this path is correct
 import App from './App.jsx';
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
